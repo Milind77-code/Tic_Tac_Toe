175e8ebea51be142d90fb2b639fbad25f85f6da9
Milind_77 <milinddhote77@gmail.com>
2021-07-21 11:36:29 +0530

    [Milind] Add:- Creating a game board.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("<--------Welcome to Tic Tac Toe Game-------->");
+            Console.WriteLine("---------------------------------------------");
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.GameBoard();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        public void GameBoard()
+        {
+            char[] board = new char[10];
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = '_';
+                Console.WriteLine(board[i]);
+            }
+        }
+    }
+}
